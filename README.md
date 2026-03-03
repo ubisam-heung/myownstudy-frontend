fcf2f5b8af023a4bb8eba087eda7bdbafb81b849
조흥재 연구원 <heung@ubisam.com>
2026-03-03 17:21:09 +0900

    20260303-4차세미나 코드

---
+++ README.md
@@ -0,0 +1,29 @@
+# Ubisam-Semina
+
+이 프로젝트는 Vuetify, Vue 3, Vite를 기반으로 한 프론트엔드 프로젝트입니다.
+
+## 프로젝트 설정
+
+```bash
+pnpm install
+```
+
+## 개발 서버 실행
+
+```bash
+pnpm dev
+```
+
+## 타입 체크 및 프로덕션 빌드
+
+```bash
+pnpm build
+```
+
+## ESLint로 코드 린트
+
+```bash
+pnpm lint
+```
+
+---
---
+++ src/pages/hello.vue
@@ -0,0 +1,33 @@
+<template>
+  Hello
+  COC철학을 따른다.
+  <div class="mt-1">
+    <v-text-field clearable label="Label" prepend-icon="$vuetify">
+    </v-text-field>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'Hello',
+
+    mounted () {
+      console.log('Hello mounted')
+      axios({
+        method: 'post',
+        url: 'http://localhost:8080/api/helloes/search',
+        data: {
+          keyword: 'hello',
+        },
+      })
+        .then(res => {
+          console.log(res)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+  }
+</script>
